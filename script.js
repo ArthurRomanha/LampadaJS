afcd54d492bd90afaa54dcf34ff8c0fde978b3d9
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-02-13 09:12:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let lampada = document.getElementById("lampada");
+let estadoLampada = "desligada";
+const ligada = () => {
+    if (!(estadoLampada == "quebrada")) {
+        lampada.src = "IMG/ligada.jpg";
+        estadoLampada = "ligada";
+    }
+}
+const desligada = () => {
+    if (!(estadoLampada == "quebrada")) {
+        lampada.src = "IMG/desligada.jpg";
+        estadoLampada = "desligada";
+    }
+}
+const quebra = () => {
+    lampada.src = "IMG/quebrada.jpg";
+    estadoLampada = "quebrada"
+}
+lampada.addEventListener("dblclick", quebra);
+lampada.addEventListener("onmouseover", )
